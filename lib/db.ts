aa83d97a10c4ebaa212ef7f3cffbbb2e78fb5685
joaoleite-2RP <joaovictor25032@gmail.com>
2026-08-31 20:04:42 -0300

    correçãodo db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: true,
   keepAlive: true,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
